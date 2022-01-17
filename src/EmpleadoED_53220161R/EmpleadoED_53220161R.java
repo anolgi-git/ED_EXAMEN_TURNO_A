710b3033beea8fe68126439cb39754caaeae86d2
AngelOlmos <anolgi@gmail.com>
2022-01-17 16:11:27 +0100

    version final con javadoc DNI 53220161R

--- src/EmpleadoED_53220161R/EmpleadoED_53220161R.java
+++ src/EmpleadoED_53220161R/EmpleadoED_53220161R.java
@@ -3,16 +3,32 @@ package EmpleadoED_53220161R;
 
 import java.util.*;
 
+/**
+ * EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+ * @author Angel Olmos Giner - 53220161R
+ * @version 1.0
+ */
 public class EmpleadoED_53220161R {
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
     
+    /**
+     * Constructor con tres parámetros
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
     public EmpleadoED_53220161R(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
-        }
+        }   
 
+    /**
+     * Calcula el salario bruto mensual en función del salario base por hora y las horas extras
+     * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora *
